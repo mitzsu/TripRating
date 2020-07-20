44ab121f372d28adead3ac1338352c954b371d6b
mitzsu <timusiiit123@gmail.com>
2020-07-20 15:17:47 +0530

    Added Trip Controller

--- src/services/apis.js
+++ src/services/apis.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const userController = require("../controller/UserController");
+const tripController = require("../controller/TripController");
 
 router.use("/user", userController);
+router.use("/trip", tripController);
 
 module.exports = router;
